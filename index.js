4c69759f6f9eae09d7016e83c44b3671796f1b24
gio-tolentino <giotolentino390@gmail.com>
2022-10-26 14:49:20 +0800

    first commit

--- index.js
+++ index.js
@@ -14,7 +14,7 @@ const io = require("socket.io")(server, {
 app.use(cors());
 
 const PORT = process.env.PORT || 5000;
-app.use(express.static('client'));
+
 app.get('/', (req, res) => {
 	res.send('Running');
 });
